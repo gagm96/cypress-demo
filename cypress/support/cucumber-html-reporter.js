19fa0dff64ce3ed1255fdbdca9bfa3f5bede361c
Gabriel Gonzalez <gabriel.gonzalez@schoolmint.net>
2024-08-20 13:31:38 -0600

    Fixed cucumber-html-reporter

--- cypress/support/cucumber-html-reporter.js
+++ cypress/support/cucumber-html-reporter.js
@@ -1,9 +1,9 @@
 const reporter = require('cucumber-html-reporter');
 
 const options = {
-  theme: 'hierarchy',
-  jsonDir: 'cypress/cucumber-json',
-  output: 'reports/html/cucumber_report.html',
+  theme: 'bootstrap',
+  jsonDir: './cypress/cucumber-json',
+  output: './cypress/reports/html/cucumber_report.html',
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
   launchReport: true,
